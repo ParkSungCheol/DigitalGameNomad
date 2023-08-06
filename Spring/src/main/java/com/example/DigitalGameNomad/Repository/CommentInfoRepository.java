e4457efed2226a226c68d729c80a6323b950533a
ParkSungCheol <skfo8gmlakd@naver.com>
2023-08-07 00:45:49 +0900

    reset

--- Spring/src/main/java/com/example/DigitalGameNomad/Repository/CommentInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DigitalGameNomad.Repository;
-
-import com.example.DigitalGameNomad.Entity.CommentInfo;
-import com.example.DigitalGameNomad.Entity.ImageInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentInfoRepository extends JpaRepository<CommentInfo, Long> {
-
-}
--- Spring/src/main/java/com/example/DigitalGameNomad/Repository/PostInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DigitalGameNomad.Repository;
-
-import com.example.DigitalGameNomad.Entity.PostInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostInfoRepository extends JpaRepository<PostInfo, Long> {
-
-    PostInfo findByPostTitle(String postTitle);
-}
